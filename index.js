dc9f9825ead2d3564e8250009d17ebe6f2d92a60
fiolam <fiola.mulliqi@wework.com>
2018-07-19 00:06:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function destructivelyRemoveFirstDriver(name){
 }
 function appendDriver(name){
   femaledriver=drivers.slice();
-  femaledriver.push(name)
+  femaledriver.push(name);
   return femaledrivers;
 }
